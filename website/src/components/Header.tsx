bf5579a579bdd79c135f7034eda95f36b171f223
MOHAMED <45185441+mohamedLazyBob@users.noreply.github.com>
2022-02-03 21:33:00 +0100

    Initialize website (#136)
    * init: initialize website
    
    initialized the folder using a next-js-boilerplate
    a small MVP of the home page
    
    * fix: fix husky instalation and missed keys
    
    Co-authored-by: Youssouf EL Azizi <youssoufelazizi@gmail.com>

---
+++ website/src/components/Header.tsx
@@ -0,0 +1,78 @@
+type IProps = {
+  pageLink: string,
+  title: string,
+  description: string,
+}
+
+const Header = ({ pageLink, title, description }: IProps) => {
+  return (
+    <div className="bg-gray-800">
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
+          <div className="flex flex-col mb-16 sm:text-center sm:mb-0">
+            <a href={pageLink} className="mb-6 sm:mx-auto">
+              <div className="flex items-center justify-center w-12 h-12 rounded-full bg-teal-accent-400">
+                <svg
+                  className="w-10 h-10 text-deep-purple-900 "
+                  stroke="currentColor"
+                  viewBox="0 0 52 52"
+                >
+                  <polygon
+                    strokeWidth="3"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    fill="none"
+                    points="29 13 14 29 25 29 23 39 38 23 27 23"
+                  />
+                </svg>
+              </div>
+            </a>
+            <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+              <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-white sm:text-4xl md:mx-auto">
+                <span className="relative inline-block">
+                  <svg
+                    viewBox="0 0 52 24"
+                    fill="currentColor"
+                    className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-deep-purple-accent-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+                  >
+                    <defs>
+                      <pattern
+                        id="700c93bf-0068-4e32-aafe-ef5b6a647708"
+                        x="0"
+                        y="0"
+                        width=".135"
+                        height=".30"
+                      >
+                        <circle cx="1" cy="1" r=".7" />
+                      </pattern>
+                    </defs>
+                    <rect
+                      fill="url(#700c93bf-0068-4e32-aafe-ef5b6a647708)"
+                      width="52"
+                      height="24"
+                    />
+                  </svg>
+                  <span className="relative">.</span>
+                </span>{' '}
+                {title}
+              </h2>
+              <p className="text-base text-indigo-100 md:text-lg">
+                {description}
+              </p>
+            </div>
+            <div>
+              <a
+                href={pageLink}
+                className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none bg-gray-900"
+              >
+                Explore
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header
