4d44dbcca108c0e831087bf1028dd2c793ade580
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2023-03-24 17:34:29 +0000

    more fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { signIn, signOut, useSession } from "next-auth/react";
-
-import { api } from "~/utils/api";
 import { NavBar, PageBanner } from "~/components";
 
 const Home: NextPage = () => {
